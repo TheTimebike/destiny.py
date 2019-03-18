78fa7ad02ef9059380d8adcf888c9900fe33573c
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-03-18 10:07:49 +0000

    Update bungieuser.py

--- destiny/bungieuser.py
+++ destiny/bungieuser.py
@@ -3,3 +3,17 @@ class BungieUser:
 		self.rawRequestData = requestData
 		
 		self.membershipID = self.rawRequestData["membershipId"]
+		
+		self.username = self.rawRequestData["uniqueName"]
+		self.display_name = self.rawRequestData["displayName"]
+		self.psn_name = self.rawRequestData["psnDisplayName"]
+		self.xbox_name = self.rawRequestData["xboxDisplayName"]
+		self.fb_name = self.rawRequestData["fbDisplayName"]		
+		
+		self.profile_picture = self.rawRequestData["profilePicture"]
+		self.profile_theme = self.rawRequestData["profileTheme"]
+		self.user_title = self.rawRequestData["userTitle"]
+		self.is_deleted = self.rawRequestData["isDeleted"]
+		self.join_date = self.rawRequestData["firstAccess"]
+		self.last_online = self.rawRequestData["lastUpdate"]
+
