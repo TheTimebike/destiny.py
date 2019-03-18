ed66034570f37ba1ae852423b9afcb25b563797d
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-03-18 09:41:51 +0000

    Update client.py

--- destiny/client.py
+++ destiny/client.py
@@ -1,16 +1,17 @@
 import asyncio
 
 from .http import GatewaySession
+from .bungieuser import BungieUser
 
 class Client:
-    BASE_ROUTE = "https://www.bungie.net/Platform/Destiny2/" # API gateway here
-    USER_ROUTE = "https://www.bungie.net/Platform/User/"
-    GROUP_ROUTE = "https://www.bungie.net/Platform/GroupV2/"
+    BASE_ROUTE = "https://www.bungie.net/Platform/Destiny2" # API gateway here
+    USER_ROUTE = "https://www.bungie.net/Platform/User"
+    GROUP_ROUTE = "https://www.bungie.net/Platform/GroupV2"
     
     def __init__(self, appName, loop=None):
         self.userAgent = None
         self.apiToken = None
-        self.loop = asyncio.get_event_loop() if loop is None else loop
+        self._loop = asyncio.get_event_loop() if loop is None else loop
         
     def run(self, apiToken):
         if self.userAgent = None:
@@ -24,3 +25,9 @@ class Client:
             self.userAgent = "{0}/{1}/{2} (+{3};{4}).format(appName, appVersion, appID, appWebsite, appEmail)"
         else:
             self.userAgent = "{0}/{1}/{2}".format(appName, appVersion, appID)
+            
+    def getUserFromID(self, userID):
+        if self.gatewaySession == None:
+            raise # Error, no client session
+        self._userData = self.gatewaySession.getRequest(USER_ROUTE + "/GetBungieNetUserById/{0}/".format(userID))
+        return BungieUser(self._userData)
