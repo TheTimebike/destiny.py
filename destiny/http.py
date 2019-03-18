add914399eb076897f1d22eef8a56b145cc71634
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-03-18 09:15:15 +0000

    Update http.py

--- destiny/http.py
+++ destiny/http.py
@@ -1,1 +1,18 @@
+import requests
 
+class GatewaySession:
+    BASE_ROUTE = "" # API gateway here
+    def __init__(self, apiToken=None, userAgent=None):
+        if apiToken == None:
+            raise # error
+            
+        self.userAgent = userAgent
+        
+        self.gatewaySession = requests.Session()
+        self.gatewaySession.headers.update({"X-API-KEY": apiToken})
+        
+    def getRequest(self, request, headers=None):
+        self.gatewaySession.get(request)
+    
+    def postRequest(self, request, headers=None):
+        self.gatewaySession.post(request)
