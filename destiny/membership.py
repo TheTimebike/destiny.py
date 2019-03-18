b83af7ad48dabd3941f29f732c6eed53f290fa25
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-03-18 10:16:25 +0000

    Update membership.py

--- destiny/membership.py
+++ destiny/membership.py
@@ -1,3 +1,14 @@
+from .userinfocard import InfoCard
+from .bungieuser import Bungieuser
+
 class Membership:
 	def __init__(self, requestData):
 		self.rawRequestData = requestData
+		
+		self._other_memberships = self.rawRequestData["destinyMemberships"]
+		self.other_memberships
+		for membershipInfoCard in self.other_memberships:
+			self.other_memberships.append(InfoCard(membershipInfoCard))
+	
+		self._userData = self.rawRequestData["bungieNetUser"]
+		self.user = BungieUser(self._userData)
