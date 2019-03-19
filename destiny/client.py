bf5bbb13b7d145340596b7176f5c85ab2dd343ee
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-03-19 17:05:53 +0000

    repaired search func and switched to aiohttp

--- destiny/client.py
+++ destiny/client.py
@@ -1,48 +1,64 @@
+import aiohttp
+import asyncio
+
 from .http import GatewaySession
 from .bungieuser import BungieUser
+from .userinfocard import InfoCard
 
 class Client:    
-    def __init__(self):
+    def __init__(self, loop=None):
         self.BASE_ROUTE = "https://www.bungie.net/Platform" # API gateway here
 
         self.userAgent = None
         self.apiToken = None
+
+        self._loop = asyncio.get_event_loop() if loop is None else loop
+        self._session = aiohttp.ClientSession(loop=self._loop)
         
+    def mainfunc(self, coro):
+        setattr(self, "mainFunction", coro)
+        print("Added mainfunction")
+
     def run(self, apiToken):
         if self.userAgent == None:
             raise SyntaxError # Error, provided no token, use setUserAgent func
         self.apiToken = apiToken
         
-        self.gatewaySession = GatewaySession(self.apiToken, self.userAgent)
-      
+        self.gatewaySession = GatewaySession(self._session, self.apiToken, self.userAgent)
+
+        self._loop.run_until_complete(self.mainFunction())
+        self._loop.close()
+
+    def close(self):
+        self._session.close()
+
     def setUserAgent(self, appName, appVersion, appID, appWebsite=None, appEmail=None):
         if appWebsite != None or appEmail != None:
             self.userAgent = "{0}/{1}/{2} (+{3};{4})".format(appName, appVersion, appID, appWebsite, appEmail)
         else:
             self.userAgent = "{0}/{1}/{2}".format(appName, appVersion, appID)
             
-    def get_user(self, bungieMembershipID):
+    async def get_user(self, bungieMembershipID):
         if self.gatewaySession == None:
             raise SyntaxError # Error, no client session
-        self._userData = self.gatewaySession.getRequest(self.BASE_ROUTE + "/Users/GetBungieNetUserById/{0}/".format(bungieMembershipID))
+        self._userData = await self.gatewaySession.getRequest(self.BASE_ROUTE + "/Users/GetBungieNetUserById/{0}/".format(bungieMembershipID))
         return BungieUser(self._userData)
 
-    def get_user_by_name(self, searchQuery):
+    async def search_for_user(self, name, membershipType=-1):
         if self.gatewaySession == None:
             raise SyntaxError # Error, no client session
-        self._possibleUsers = self.gatewaySession.getRequest(self.BASE_ROUTE + "/Users/SearchUsers/{0}".format(searchQuery))
+        self._possibleUsers = await self.gatewaySession.getRequest(self.BASE_ROUTE + "/Destiny2/SearchDestinyPlayer/{1}/{0}".format(membershipType, name))
         self._userObjectList = []
-        for bungieUserData in self._possibleUsers:
-            self._userObjectList.append(BungieUser(bungieUserData))
+        for bungieUserData in self._possibleUsers["Response"]:
+            self._userObjectList.append(InfoCard(bungieUserData))
         return self._userObjectList
     
-    def get_membership_type(self, bungieMembershipID):
+    async def get_membership_type(self, bungieMembershipID):
         if self.gatewaySession == None:
             raise SyntaxError # Error, no client session
     
-    def get_manifest(self):
+    async def get_manifest(self):
         if self.gatewaySession == None:
             raise SyntaxError # Error, no client session
-        self._manifest = self.gatewaySession.getRequest(self.BASE_ROUTE + "/Destiny2/Manifest/")
-        return self._manifest
+        return await self.gatewaySession.getRequest(self.BASE_ROUTE + "/Destiny2/Manifest/")
         
--- destiny/http.py
+++ destiny/http.py
@@ -1,24 +1,27 @@
-import requests
+import aiohttp
+import asyncio
 
 class GatewaySession:
     BASE_ROUTE = "https://www.bungie.net/Platform/Destiny2/" # API gateway here
     USER_ROUTE = "https://www.bungie.net/Platform/User/"
     GROUP_ROUTE = "https://www.bungie.net/Platform/GroupV2/"
     
-    def __init__(self, apiToken=None, userAgent=None):
+    def __init__(self, session, apiToken=None, userAgent=None):
         if apiToken == None:
             raise # error
             
         self.userAgent = userAgent
+        self.session = session
+        self.headers = {"X-API-Key": apiToken}
         
-        self.gatewaySession = requests.Session()
-        self.gatewaySession.headers.update({"X-API-Key": apiToken})
-        
-    def getRequest(self, request, headers=None):
-        print(request)
-        self._requestData = self.gatewaySession.get(request)
-        print(self._requestData)
-        return self._requestData.json()
+    async def getRequest(self, request):
+        try:
+            async with self.session.get(request, headers=self.headers) as _data:
+                self._requestData = await _data.json()
+                #print(self._requestData)
+            return self._requestData
+        except Exception as ex:
+            #print(ex)
     
-    def postRequest(self, request, headers=None):
-        self._requestData = self.gatewaySession.post(request).json()
+    async def postRequest(self, request):
+        self._requestData = self.session.post(request, headers=self.headers).json()
