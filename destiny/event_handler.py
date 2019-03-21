2f7d459a9a58cfdc77e13e7ba3d1f4f739ba5ac8
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-03-21 11:20:06 +0000

    Update event_handler.py

--- destiny/event_handler.py
+++ destiny/event_handler.py
@@ -6,9 +6,14 @@ class EventHandler:
         if hasattr(self, "on_run"):
             self.on_run()
             
-    def _trigger_on_recieve(self):
+    def _trigger_on_recieve(self, recieved):
         if hasattr(self, "on_recieve"):
-            self.on_recieve()
+            self.on_recieve(recieved)
             
-    def _register_authorisation(self):
-    
+    def _trigger_on_get_request(self, request):
+        if hasattr(self, "on_get_request"):
+            self.on_get_request(request)
+            
+    def _trigger_on_post_request(self):
+        if hasattr(self, "on_post_request"):
+            self.on_post_request(request)
