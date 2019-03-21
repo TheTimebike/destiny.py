ae98bd6e4824e6b8b7f895f90d03073f49d94000
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-03-21 11:32:38 +0000

    Update event_handler.py

--- destiny/event_handler.py
+++ destiny/event_handler.py
@@ -2,21 +2,21 @@ class EventHandler:
     def __init__(self, focus):
         pass
     
-    def _trigger_event(self, event):
-        self.focus._loop._run_until_complete( getattr(self, event)() )
+    def _trigger_event(self, event, **args):
+        self.focus._loop._run_until_complete( getattr(self, event)(args) )
     
     async def _trigger_on_run(self):
         if hasattr(self, "on_run"):
             await self.on_run()
             
-    async def _trigger_on_recieve(self, recieved):
+    async def _trigger_on_recieve(self, params):
         if hasattr(self, "on_recieve"):
-            await self.on_recieve(recieved)
+            await self.on_recieve(params[0])
             
-    async def _trigger_on_get_request(self, request):
+    async def _trigger_on_get_request(self, params):
         if hasattr(self, "on_get_request"):
-            await self.on_get_request(request)
+            await self.on_get_request(params[0])
             
-    async def _trigger_on_post_request(self):
+    async def _trigger_on_post_request(self, params):
         if hasattr(self, "on_post_request"):
-            await self.on_post_request(request)
+            await self.on_post_request(params[0])
