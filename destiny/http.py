3b734ff80c59c3af5c0704b8c699cff228b896bc
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-03-21 11:22:11 +0000

    Update http.py

--- destiny/http.py
+++ destiny/http.py
@@ -11,26 +11,30 @@ class GatewaySession:
     USER_ROUTE = "https://www.bungie.net/Platform/User/"
     GROUP_ROUTE = "https://www.bungie.net/Platform/GroupV2/"
     
-    def __init__(self, session, apiToken=None, userAgent=None):
-        if apiToken == None:
+    def __init__(self, session, eventHandler=None, apiToken=None, userAgent=None):
+        if apiToken == None or eventHandler == None:
             raise # error
             
         self.userAgent = userAgent
         self.session = session
         self.headers = {"X-API-Key": apiToken}
+        self._event_handler = eventHandler
         
     async def getRequest(self, request):
         try:
+            self._event_handler._trigger_on_get_request(request)
             async with self.session.get(request, headers=self.headers) as _data:
                 self._requestData = await _data.json()
-            with open("./Logs.json", "a+") as out:
-                withUrl = self._requestData
-                withUrl["request"] = request
-                json.dump(withUrl, out, indent=4)
+            self._event_handler._trigger_on_recieve(request)
+            #with open("./Logs.json", "a+") as out:
+            #    withUrl = self._requestData
+            #    withUrl["request"] = request
+            #    json.dump(withUrl, out, indent=4)
             return self._requestData
         except Exception as ex:
             print(ex)
             raise HTTPException
     
     async def postRequest(self, request):
+        self._event_handler._trigger_on_post_request(request)
         self._requestData = self.session.post(request, headers=self.headers).json()
