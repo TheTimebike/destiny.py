7a030b089d342b8db362fa3e7237c34fb823dd54
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-03-21 11:29:01 +0000

    Update client.py

--- destiny/client.py
+++ destiny/client.py
@@ -36,7 +36,7 @@ class Client:
         self.apiToken = self.auth.apiToken 
         self.gatewaySession = GatewaySession(self._session, self.apiToken, self.userAgent)
 
-        self._loop.run_until_complete(self._event_handler._trigger_on_run())
+        self._loop.run_until_complete(self._event_handler._trigger_event("_trigger_on_run"))
 
     def close(self):
         self._session.close()
