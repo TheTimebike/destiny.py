891389776edbf3ba30e259a3245274511a026061
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-03-23 12:23:48 +0000

    fixed variable naming inconsistency

--- destiny/components.py
+++ destiny/components.py
@@ -2,14 +2,14 @@ class Components:
     """Represents the data given back by API requests.
     Attributes will be added to this class depending on what components are requested to it.
 
-    component_list
+    components
         This is a list of components that the object currently has stored.
     
     """    
     def __init__(self):
-        self.component_list = []
+        self.components = []
         pass
 
     def _add_attr(self, name, component):
         setattr(self, name, component)
-        self.component_list.append(name)
+        self.components.append(name)
