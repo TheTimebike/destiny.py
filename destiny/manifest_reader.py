92fc0b3b3ce859f674fe8eadcc8d62c9bac0b1b2
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-03-26 13:16:47 +0000

    Update manifest_reader.py

--- destiny/manifest_reader.py
+++ destiny/manifest_reader.py
@@ -1,3 +1,14 @@
 import sqlite3
 
 class ManifestReader:
+	def __init__(self, manifestFile):
+		self.connection = sqlite3.connect(manifestFile)
+		self.cursor = self.connection.cursor()
+		
+	def query(self, hash, definiton, identifier):
+		sql = """
+			  SELECT json from {0}
+			  WHERE {1} = {2}
+			  """.format(definition, identifier, hash)
+		self.cursor.execute(sql)
+		return self.cursor.fetchall()
