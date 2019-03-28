682c1d570fa017f2e882f879f9d0e4ab61f7fb82
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-03-28 13:22:27 +0000

    Update milestone.py

--- destiny/milestone.py
+++ destiny/milestone.py
@@ -1,1 +1,9 @@
-
+class Milestone:
+	def __init__(self, client, milestoneHash):
+		_responseData = await client.decode_hash(milestoneHash "DestinyMilestoneDefinition", "en")
+		self.milestoneHash = milestoneHash
+		if self.displayName = _responseData.get("displayProperties", None) != None:
+			self.displayName = _responseData["displayProperties"].get("name", None)
+			self.description = _responseData["displayProperties"].get("description", None)
+			self.hasIcon = _responseData["displayProperties"].get("hasIcon", None)
+			self.icon = _responseData["displayProperties"].get("icon", None)
