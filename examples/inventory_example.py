cad558b3856d1d755feb0080cc0787226da44a04
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-03-28 08:55:44 +0000

    fixed variable names and commented for easier understanding

--- examples/inventory_example.py
+++ examples/inventory_example.py
@@ -12,23 +12,26 @@ async def on_run():
         "4611686018468394612",
         4,
         components=[
-            "ProfileInventories",
-            "CharacterInventories",
-            "CharacterEquipment"
+            "ProfileInventories", # To get items in the vault
+            "CharacterInventories", # To get items on your character
+            "CharacterEquipment" # To get equipped items.
             ]
     )
-    table = {}
+    itemTable = {}
     itemList = myProfile.profileInventory["items"]
+    
     for key, attr in myProfile.characterInventories.items():
-        itemList += attr["items"]
+        itemList += attr["items"] # Iterate through the characters and add the results to the itemList
+        
     for key, attr in myProfile.characterEquipment.items():
         itemList += attr["items"]
+        
     for item in itemList:
         try:
-            thing = await client.decode_hash(item["itemHash"], "DestinyInventoryItemDefinition", "en")
-            if table.get(thing["itemTypeAndTierDisplayName"], None) == None:
-                table[thing["itemTypeAndTierDisplayName"]] = []
-            table[thing["itemTypeAndTierDisplayName"]].append(thing["displayProperties"])
+            itemData = await client.decode_hash(item["itemHash"], "DestinyInventoryItemDefinition", "en") # Decode the item hash into its data
+            if itemTable.get(itemData["itemTypeAndTierDisplayName"], None) == None:
+                itemTable[itemData["itemTypeAndTierDisplayName"]] = []
+            itemTable[itemData["itemTypeAndTierDisplayName"]].append(itemData["displayProperties"])
         except Exception as ex:
             print(ex)
     with open("inventory.json", "w+") as out:
